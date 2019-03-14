ecd16a0e005abe53869c15d688ed75dc40de11b4
Atanas Kostov <ankostov@gmail.com>
2019-03-14 10:27:34 +0200

    first commit
    first commit

---
+++ A/A/Cat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A
+{
+    class Cat
+    {
+        public string color;
+        public string name;
+
+        public void SayMiau()
+        {
+            Console.WriteLine("Miau");
+        }
+    }
+}
---
+++ A/A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat1 = new Cat();
+            cat1.SayMiau();
+            Console.ReadKey();
+        }
+    }
+}
